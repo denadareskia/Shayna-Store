b0b537e83e97c11bbffa2cc4c2bfb72b13303aec
Denada Reskia <denada.reskia@gmail.com>
2022-09-06 18:36:45 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderComponent/>
+    <HeroComponent />
+    <ProductCarousel />
+    <InstaComponent />
+    <PartnerComponent />
+    <FooterComponent />
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+ import HeaderComponent from '@/components/HeaderComponent.vue'
+ import HeroComponent from '@/components/HeroComponent.vue'
+ import ProductCarousel from '@/components/ProductCarousel.vue'
+ import InstaComponent from '@/components/InstaComponent.vue'
+ import PartnerComponent from '@/components/PartnerComponent.vue'
+ import FooterComponent from '@/components/FooterComponent.vue'
+
+export default {
+  name: "HomeView",
+  components: {
+    HeaderComponent,
+    HeroComponent,
+    ProductCarousel,
+    InstaComponent,
+    PartnerComponent,
+    FooterComponent
+  },
+};
+</script>
